650dd3795abe3dfe22355d33cf45b789a7db5ca7
sharmaricha7122020 <sharmaricha7122020@gmail.com>
2022-07-12 11:10:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+       <img src={logo} className="App-logo" alt="logo" width={'200'} height={'200'}/>
+       <h1>Hello My First React Js App</h1>
+    </div>
+  );
+}
+
+export default App;
